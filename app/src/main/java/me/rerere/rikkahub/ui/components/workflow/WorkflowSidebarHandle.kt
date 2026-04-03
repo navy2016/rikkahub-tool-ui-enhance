e83c197d3970752ea24e81584ecad186bf6c1a53
navy <2603451187@qq.com>
2026-04-03 21:09:34 +0800

    fix: Add missing mutableStateOf import

--- app/src/main/java/me/rerere/rikkahub/ui/components/workflow/WorkflowSidebarHandle.kt
+++ app/src/main/java/me/rerere/rikkahub/ui/components/workflow/WorkflowSidebarHandle.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
